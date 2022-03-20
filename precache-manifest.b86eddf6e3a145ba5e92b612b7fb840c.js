8360ef54369090e725fe4eeeea5fc287c80d95c4
miracle <poppymanye@naver.com>
2022-03-20 19:15:22 +0900

    Updates

---
+++ precache-manifest.b86eddf6e3a145ba5e92b612b7fb840c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f29df854e40e37c2ac45ebc4b549fe3",
+    "url": "/folio.github.io/index.html"
+  },
+  {
+    "revision": "f5cca069ca29a5541eba",
+    "url": "/folio.github.io/static/css/main.f77d7a38.chunk.css"
+  },
+  {
+    "revision": "f0585afd58bab4c12498",
+    "url": "/folio.github.io/static/js/2.9a2180c5.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/folio.github.io/static/js/2.9a2180c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5cca069ca29a5541eba",
+    "url": "/folio.github.io/static/js/main.4aa331de.chunk.js"
+  },
+  {
+    "revision": "6f5d2d96cb21505768ac",
+    "url": "/folio.github.io/static/js/runtime-main.235dcccb.js"
+  },
+  {
+    "revision": "b88924d5d914bf9f3885f87c2ed76477",
+    "url": "/folio.github.io/static/media/sad-icon.b88924d5.png"
+  },
+  {
+    "revision": "b9d7d448a09d6aacb45d21aab2c6fd53",
+    "url": "/folio.github.io/static/media/your photo.b9d7d448.png"
+  }
+]);
